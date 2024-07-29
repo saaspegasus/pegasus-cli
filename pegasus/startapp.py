c4bd2dba875c6efa586723df0c6f2acd83243e26
Simon Kelly <skelly@dimagi.com>
2024-07-29 11:50:58 +0200

    beginnings of 'startapp'

---
+++ pegasus/startapp.py
@@ -0,0 +1,33 @@
+import pathlib
+
+import click
+
+
+def validate_name(ctx, param, value):
+    if not value.isidentifier():
+        raise click.BadParameter(
+            f"'{value}' is not a valid app name. Please make sure it is a valid identifier."
+        )
+    return value
+
+
+@click.command(name="startapp")
+@click.argument("name", callback=validate_name)
+@click.argument(
+    "directory",
+    envvar="APP_DIRECTORY",
+    type=click.Path(file_okay=False, exists=True, resolve_path=True),
+    default=".",
+)
+def startapp(name, directory):
+    """Creates a Django app directory structure for the given app name in
+    the current directory or optionally in the given directory.
+
+    NAME is the name of the Django app
+    DIRECTORY is the path of the directory to create the app in. Defaults to the current directory.
+    """
+    app_dir = pathlib.Path(directory) / name
+    if not app_dir.exists():
+        app_dir.mkdir()
+    elif any(app_dir.iterdir()):
+        raise click.ClickException(f"target directory must be empty: {app_dir}")
