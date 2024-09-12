32fb9fa7fe2b6ee4c5ac6fae98846a010f219f48
Cory Zue <cory@coryzue.com>
2024-09-12 15:20:25 +0200

    read values from config

--- pegasus/startapp.py
+++ pegasus/startapp.py
@@ -1,4 +1,5 @@
 import pathlib
+import yaml
 
 import click
 
@@ -13,43 +14,90 @@ def validate_name(ctx, param, value):
     return value
 
 
+def validate_model_name(ctx, param, value):
+    if not value.isidentifier() or not value[0].isupper():
+        raise click.BadParameter(
+            f"'{value}' is not a valid Django model name. "
+            "It should be a valid Python identifier and start with an uppercase letter."
+        )
+    return value
+
+
+def load_config(ctx, param, value):
+    if value is None:
+        default_config = pathlib.Path.cwd() / "pegasus-config.yaml"
+        print("default_config is", default_config)
+        if default_config.exists():
+            value = str(default_config)
+        else:
+            return {}
+    try:
+        print("loading config from", value)
+        with open(value, "r") as config_file:
+            config = yaml.safe_load(config_file)
+            if config.get("cli"):
+                config = config["cli"]
+            return config
+    except Exception as e:
+        raise click.BadParameter(f"Error loading config file: {str(e)}")
+
+
 @click.command(name="startapp")
 @click.argument("name", callback=validate_name)
-@click.argument(
-    "directory",
-    envvar="PEGASUS_APP_DIRECTORY",
-    type=click.Path(file_okay=False, exists=True, resolve_path=True),
-    default=".",
-)
-@click.argument(
-    "module_path",
-    envvar="PEGASUS_MODULE_PATH",
-    type=click.STRING,
-    default="",
-)
 @click.argument(
     "model_name",
     envvar="PEGASUS_MODEL_NAME",
     type=click.STRING,
-    default="",
+    callback=validate_model_name,
 )
-@click.argument(
-    "template_directory",
+@click.option(
+    "--config",
+    type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+    callback=load_config,
+    help="Path to YAML config file (default: ./pegasus-config.yml)",
+)
+@click.option(
+    "--app-directory",
+    envvar="PEGASUS_APP_DIRECTORY",
+    type=click.Path(file_okay=False, exists=True, resolve_path=True),
+    default=".",
+    help="Directory to create the app in",
+)
+@click.option(
+    "--module-path",
+    envvar="PEGASUS_MODULE_PATH",
+    type=click.STRING,
+    default="",
+    help="Namespace of the module to create the app in",
+)
+@click.option(
+    "--template-directory",
     envvar="PEGASUS_TEMPLATE_DIRECTORY",
     type=click.Path(file_okay=False, exists=True, resolve_path=False),
     default=".",
+    help="Directory containing templates",
 )
-def startapp(name, directory, module_path, model_name, template_directory):
+def startapp(name, model_name, config, app_directory, module_path, template_directory):
     """Creates a Django app directory structure for the given app name in
     the current directory or optionally in the given directory.
 
     \b
     NAME is the name of the Django app
-    DIRECTORY is the path of the directory to create the app in. Defaults to the current directory.
-    MODULE_PATH is the namespace of the module to create the app in. Defaults to "".
-    MODEL_NAME is the name of the model to create. Defaults to "" (don't create a model).
+    MODEL_NAME is the name of the Django model
     """
-    app_dir = pathlib.Path(directory) / name
+    # Override CLI options with config file values if present
+    print("config is", config)
+    app_directory = config.get("app_directory", app_directory)
+    module_path = config.get("module_path", module_path)
+    model_name = config.get("model_name", model_name)
+    template_directory = config.get("template_directory", template_directory)
+
+    print("app_directory is", app_directory)
+    print("module_path is", module_path)
+    print("model_name is", model_name)
+    print("template_directory is", template_directory)
+
+    app_dir = pathlib.Path(app_directory) / name
     if not app_dir.exists():
         app_dir.mkdir()
     elif any(app_dir.iterdir()):
