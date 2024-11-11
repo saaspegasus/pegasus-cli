3e541a006935a537f1df5c412ca4a18af9a2cce3
Cory Zue <cory@coryzue.com>
2024-11-11 13:45:36 +0200

    poc for cookiecutter startproject

--- pegasus_cli/startproject.py
+++ pegasus_cli/startproject.py
@@ -1,6 +1,8 @@
 import click
-import os
+
+from pegasus_cli.monkeypatch import patch_cookiecutter
 from .config import apply_shared_options, get_shared_options
+from .generate import render_cookiecutter
 
 
 @click.command()
@@ -8,19 +10,37 @@ from .config import apply_shared_options, get_shared_options
 @apply_shared_options(get_shared_options())
 def startproject(project_name, config):
     """
-    Create a new Django project with the given name
+    Create a new Django project with the given name using local templates
     """
-    try:
-        # Use config values if present
-        template_directory = config.get("template_directory", ".")
 
-        if template_directory != "." and os.path.exists(template_directory):
-            os.system(
-                f"django-admin startproject {project_name} --template={template_directory}"
-            )
-        else:
-            os.system(f"django-admin startproject {project_name}")
+    context = {
+        "project_name": project_name,
+        "camel_case_project_name": "".join(x for x in project_name.title() if x != "_"),
+    }
 
-        click.echo(f"Successfully created project: {project_name}")
-    except Exception as e:
-        click.echo(f"Error creating project: {e}", err=True)
+    extra_cookiecutter_context = {
+        "project_name": project_name,
+    }
+    print(context)
+    print(extra_cookiecutter_context)
+    patch_cookiecutter()
+    click.echo(f"Creating project: {project_name}")
+
+    render_cookiecutter(
+        "project_template",
+        ".",
+        context,
+        extra_cookiecutter_context,
+    )
+    # render_cookiecutter(
+    #     "project_template",
+    #     ".",
+    #     context,
+    #     extra_cookiecutter_context,
+    # )
+
+    click.echo(f"Successfully created project: {project_name}")
+    click.echo("To get started:")
+    click.echo(f"  cd {project_name}")
+    click.echo("  python manage.py migrate")
+    click.echo("  python manage.py runserver")
