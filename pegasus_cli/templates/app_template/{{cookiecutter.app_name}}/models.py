a6d8f1ce614d5273ee84868a00d89cced1c75994
Cory Zue <cory@coryzue.com>
2024-11-27 10:11:24 +0200

    add default __str__ function

--- pegasus_cli/templates/app_template/{{cookiecutter.app_name}}/models.py
+++ pegasus_cli/templates/app_template/{{cookiecutter.app_name}}/models.py
@@ -13,6 +13,9 @@ class << model_name >>(<<base_model_class if base_model else "models.Model">>):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
     def get_absolute_url(self):
         return reverse("<< app_name >>:<< model_name | lower >>_detail", kwargs={<% if extra_view_param %>"<< extra_view_param >>": << extra_model_param_value >>, <% endif %>"pk": self.pk})
 <%- endfor %>
