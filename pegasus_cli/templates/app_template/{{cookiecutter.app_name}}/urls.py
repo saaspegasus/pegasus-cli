fd2c73d545be03514e2e52e5df6e5074b34d64b1
Cory Zue <cory@coryzue.com>
2025-12-12 15:46:17 +0200

    pluralize urls

--- pegasus_cli/templates/app_template/{{cookiecutter.app_name}}/urls.py
+++ pegasus_cli/templates/app_template/{{cookiecutter.app_name}}/urls.py
@@ -7,10 +7,10 @@ app_name = "<< app_name >>"
 urlpatterns = [
     path("", views.home, name="<< app_name >>_home"),
 <%- for model_name in model_names %>
-    path("<< model_name | lower >>/", views.<< model_name | lower >>_list, name="<< model_name | lower >>_list"),
-    path("<< model_name | lower >>/<int:pk>/", views.<< model_name | lower >>_detail, name="<< model_name | lower >>_detail"),
-    path("<< model_name | lower >>/create/", views.<< model_name | lower >>_create, name="<< model_name | lower >>_create"),
-    path("<< model_name | lower >>/<int:pk>/update/", views.<< model_name | lower >>_update, name="<< model_name | lower >>_update"),
-    path("<< model_name | lower >>/<int:pk>/delete/", views.<< model_name | lower >>_delete, name="<< model_name | lower >>_delete"),
+    path("<< model_name | lower >>s/", views.<< model_name | lower >>_list, name="<< model_name | lower >>_list"),
+    path("<< model_name | lower >>s/<int:pk>/", views.<< model_name | lower >>_detail, name="<< model_name | lower >>_detail"),
+    path("<< model_name | lower >>s/create/", views.<< model_name | lower >>_create, name="<< model_name | lower >>_create"),
+    path("<< model_name | lower >>s/<int:pk>/update/", views.<< model_name | lower >>_update, name="<< model_name | lower >>_update"),
+    path("<< model_name | lower >>s/<int:pk>/delete/", views.<< model_name | lower >>_delete, name="<< model_name | lower >>_delete"),
 <%- endfor %>
 ]
